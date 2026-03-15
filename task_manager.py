7fb54b5cb3d43de61daf11597824672c45eea9e5
Aaron <aaronmjameson00@gmail.com>
2026-03-15 02:19:16 -0400

    Update task_manager.py

--- task_manager.py
+++ task_manager.py
@@ -1,32 +1,41 @@
 #Weclome messaage
 print('Welcome to your Task Manager')
-tasks_list = []
+task_list = []
+
+#Task Variables for strings
+open_question = """                 
+What would you like to do?
+View Task. Type View
+Add Task. Type Add
+Complete Task. Type Complete
+""")
+add_task = "What task would you like to add?\n "
+remove_task = "What task would you like to complete/remove?\n "
+new_task = "Got it! Here is your new task list."
+update_task = "Good job completing that! Here is an updated list for you."
+invalid_response = "I'm sorry. I don't enderstand your response.\nPlease try again."
 
 #need to add a task_list remove option as well.
 
-task_response = input("""What would you like to do?
-                         View Tasks. Type View
-                         Add Task. Type Add
-                         Complete Task. Type Complete
-                         """).lower()
+task_response = input(open_question).lower()
 
 while task_response == "view" or "edit":
 
   if task_response == "view"
     print(task_list)
   elif task_response == "add"
-    task_list,append(input("What task would you like to add?\n ")
-    print("Got it! Here is your new task list.")
+    task_list.append(input(add_task).lower()
+    print(new_task)
     print(task_list)
   else:
     print(task_list)
-    task_list.remove(input("What task would you like to complete/remove?\n").lower()
-    print("Good job completing that! Here is an updated list for you.")
+    task_list.remove(input(remove_task).lower()
+    print(update_task)
     print(task_list)
 if task_response == "exit"
     # save data and close program
 else:
-  print("I'm sorry. I don't enderstand your response.\nPlease try again.")
+  print(invalid_response)
   #repeat line 7
   
                     
